c80bdd17521404ac156db97b8b3be31844d9bf65
Gethyn Jones <gethyn1@gmail.com>
2019-10-10 12:59:40 +0100

    Remove redundant types

--- src/app/state/location/types.ts
+++ src/app/state/location/types.ts
@@ -1,9 +1,9 @@
 import { FetchActionTypes } from '../../types/fetch-meta.interface'
 
-const namespace: string = 'claim'
+const namespace = 'claim'
 
-export const READ_CLAIM_REQUESTED: string = `${namespace}/READ_CLAIM_REQUESTED`
-export const READ_CLAIM_SUCCEEDED: string = `${namespace}/READ_CLAIM_SUCCEEDED`
-export const READ_CLAIM_FAILED: string = `${namespace}/READ_CLAIM_FAILED`
+export const READ_CLAIM_REQUESTED = `${namespace}/READ_CLAIM_REQUESTED`
+export const READ_CLAIM_SUCCEEDED = `${namespace}/READ_CLAIM_SUCCEEDED`
+export const READ_CLAIM_FAILED = `${namespace}/READ_CLAIM_FAILED`
 
 export const READ: FetchActionTypes = [READ_CLAIM_REQUESTED, READ_CLAIM_SUCCEEDED, READ_CLAIM_FAILED]
--- src/app/state/locations/types.ts
+++ src/app/state/locations/types.ts
@@ -1,9 +1,9 @@
 import { FetchActionTypes } from '../../types/fetch-meta.interface'
 
-const namespace: string = 'locations'
+const namespace = 'locations'
 
-export const READ_LOCATIONS_REQUESTED: string = `${namespace}/READ_LOCATIONS_REQUESTED`
-export const READ_LOCATIONS_SUCCEEDED: string = `${namespace}/READ_LOCATIONS_SUCCEEDED`
-export const READ_LOCATIONS_FAILED: string = `${namespace}/READ_LOCATIONS_FAILED`
+export const READ_LOCATIONS_REQUESTED = `${namespace}/READ_LOCATIONS_REQUESTED`
+export const READ_LOCATIONS_SUCCEEDED = `${namespace}/READ_LOCATIONS_SUCCEEDED`
+export const READ_LOCATIONS_FAILED = `${namespace}/READ_LOCATIONS_FAILED`
 
 export const READ: FetchActionTypes = [READ_LOCATIONS_REQUESTED, READ_LOCATIONS_SUCCEEDED, READ_LOCATIONS_FAILED]
