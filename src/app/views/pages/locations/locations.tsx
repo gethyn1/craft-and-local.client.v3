2a3eaf9a93f4a3cd894c595e8f78c367af9f2acd
Gethyn Jones <gethyn1@gmail.com>
2019-10-26 18:36:42 +0100

    Render locations component by meta

--- src/app/views/pages/locations/locations.tsx
+++ src/app/views/pages/locations/locations.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { AppLayout } from '../../layouts'
 import { PopulatedLocation } from '../../../types/location.interface'
-import { FetchMeta } from '../../../types/fetch-meta.interface'
+import { CrudMeta } from '../../../types/fetch-meta.interface'
 import { Grid } from '../../../components/grid'
 import { Heading, Headings, Levels } from '../../../components/heading'
 import { Container } from '../../../components/container'
@@ -9,6 +9,9 @@ import { Box } from '../../../components/box'
 import { Sizes } from '../../../components/enums'
 import { LocationCard } from './location-card'
 import { GetLocations } from './types'
+import { Skeleton } from '../../../components/skeleton'
+import { Card } from '../../../components/card'
+import { ComponentByMeta } from '../../common/component-by-meta'
 
 const { useEffect } = React
 
@@ -16,7 +19,7 @@ interface LocationsProps {
   getLocations: ({ coordinates }: GetLocations) => void,
   getCategories: () => void,
   locations: PopulatedLocation[],
-  meta: FetchMeta
+  meta: CrudMeta
 }
 
 const getUserLocationCoordinates = (): Promise<{ latitude: number, longitude: number }> =>
@@ -33,6 +36,22 @@ const getUserLocationCoordinates = (): Promise<{ latitude: number, longitude: nu
     }
   })
 
+const LocationsSkeleton = ({ size }: { size: number }) => {
+  const items = Array(size).fill(() => <Card size={Sizes.MEDIUM}><Skeleton /></Card>)
+
+  return (
+    <Grid>
+      {items.map((Item, i) => <Item key={i} />)}
+    </Grid>
+  )
+}
+
+const LocationsGrid = ({ locations }: { locations: PopulatedLocation[] }) => (
+  <Grid>
+    {locations.map(location => <LocationCard key={location.id} location={location} />)}
+  </Grid>
+)
+
 const Locations = ({ getLocations, getCategories, locations, meta }: LocationsProps) => {
   useEffect(() => {
     getCategories()
@@ -46,10 +65,12 @@ const Locations = ({ getLocations, getCategories, locations, meta }: LocationsPr
       <Container>
         <Box size={Sizes.LARGE}>
           <Heading as={Headings.H1} level={Levels.LEVEL_2}>Locations</Heading>
-          {meta.read.hasErrored && <p>There was an error loading locations</p>}
-          <Grid>
-            {locations.map(location => <LocationCard key={location.id} location={location} />)}
-          </Grid>
+          <ComponentByMeta
+            hasLoaded={<LocationsGrid locations={locations} />}
+            isLoading={<LocationsSkeleton size={4} />}
+            hasErrored={<p>There was an error loading locations</p>}
+            meta={meta.read}
+          />
         </Box>
       </Container>
     </AppLayout>
