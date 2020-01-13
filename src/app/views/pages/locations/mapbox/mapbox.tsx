00b80ef80f6014b7199f6c068ce9762d9933bbba
Gethyn Jones <gethyn1@gmail.com>
2020-01-13 20:09:14 +0000

    Track markers for Mapbox map instance

--- src/app/views/pages/locations/mapbox/mapbox.tsx
+++ src/app/views/pages/locations/mapbox/mapbox.tsx
@@ -11,12 +11,17 @@ type MapProps = {
   locations: Location[],
 }
 
+// TODO This is a bit of a hack for keeping track of map markers:
+// Create a prop for each initialised map to track markers for removal
+// Might be better to create a class component
+const memoMarkers = {}
+
 const MapBox = ({ center, locations }: MapProps) => {
   const mapElement = useRef(null)
   const [map, setMap] = useState(null)
   const [apiLoaded, apiError] = useScript(MAPBOX_API_URL)
 
-  if (apiLoaded && !map && center) {
+  if (apiLoaded && !map && center && window['mapboxgl']) {
     window['mapboxgl'].accessToken = process.env.MAPBOX_TOKEN
 
     setMap(new window['mapboxgl'].Map({
@@ -28,23 +33,34 @@ const MapBox = ({ center, locations }: MapProps) => {
     }))
   }
 
-  if (map && locations.length) {
-    const bounds = new window['mapboxgl'].LngLatBounds()
+  useEffect(() => {
+    if (map && locations.length) {
+      // TODO Could use Map._mapId instead of creating UID
+      const { _mapId } = map
+      if (!memoMarkers[_mapId]) {
+        memoMarkers[_mapId] = []
+      }
 
-    locations.forEach(location => {
-      const { coordinates } = location.location
-      const marker = new window['mapboxgl'].Marker()
-        .setLngLat(coordinates)
-        .addTo(map)
+      memoMarkers[_mapId].forEach(marker => marker.remove())
+      memoMarkers[_mapId] = []
+      const bounds = new window['mapboxgl'].LngLatBounds()
 
-      bounds.extend(coordinates)
-    })
+      locations.forEach(location => {
+        const { coordinates } = location.location
+        const marker = new window['mapboxgl'].Marker()
+          .setLngLat(coordinates)
+          .addTo(map)
 
-    map.fitBounds(bounds, {
-      padding: { top: 50, bottom: 50, left: 50, right: 50 },
-      easing: (t: number) => t * (2 - t)
-    })
-  }
+          memoMarkers[_mapId] = [...memoMarkers[_mapId], marker]
+        bounds.extend(coordinates)
+      })
+
+      map.fitBounds(bounds, {
+        padding: { top: 50, bottom: 50, left: 50, right: 50 },
+        easing: (t: number) => t * (2 - t)
+      })
+    }
+  }, [locations])
 
   return apiError
     ? <p>There was an error loading the map</p>
